f2cfd1996370ef9d36d412c0111e8e3ea46a8913
Shaylee21 <shayleepillay021@gmail.com>
2023-04-24 06:09:45 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,50 +1,51 @@
 #include "main.h"
 
-/**
- * _printf - Produces output according to a format.
- * @format: Format
- * Return: Number of charatcters printed
- */
+void print_buffer(char buffer[], int *buff_ind);
 
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-	int x, printed = 0, print_ch = 0;
-	int flg, w, p, size, bu = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
-	return (-1);
+		return (-1);
 
 	va_start(list, format);
 
-	for (x = 0; format && format[x] != '\0'; x++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[x] != '%')
+		if (format[i] != '%')
 		{
-			buffer[bu++] = format[x];
-			if (bu == BUFF_SIZE)
-				print_buffer(buffer, &bu);
-			/* write(1, &format[x], 1);*/
-			print_ch++;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &bu);
-			flg = get_flg(format, &x);
-			w = get_w(format, &x, list);
-			p = get_p(format, &x, list);
-			size = get_size(format, &x);
-			++x;
-			printed = handle_print(format, &x, list, buffer,
-				flg, w, p, size);
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
-			print_ch += printed;
+			printed_chars += printed;
 		}
 	}
 
-	print_buffer(buffer, &bu);
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
